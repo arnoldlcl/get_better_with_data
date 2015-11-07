790a7b8c607656512cdf2599697baf51ba3efdc3
arnoldlcl <arnoldlindlau@gmail.com>
2015-11-07 13:57:30 -0500

    data.table, fread, etc

--- rmysql_alcl.R
+++ rmysql_alcl.R
@@ -1,4 +1,31 @@
-genpay <- read.csv("CMS 2014/CMS 2014 General Payments Details.csv", nrow = 100)
+setwd("D:/GBWD")
+
+library(data.table)
+
+genpay <- fread("CMS 2014/CMS 2014 General Payments Details.csv", colClasses = c(rep("character", 23), rep("NULL", 6), "character", rep("NULL", 33)))
+genpay_phys <- genpay[Teaching_Hospital_ID == "", ]
+
+write_genpay_phys <- function(genpay_phys) {
+  for(i in 1:107) {
+    write.table(genpay_phys[i:(i + 99999), ], "CMS 2014 General Payments Details - Physician Info.csv", append = TRUE, sep = ",")
+  }
+}
+
+genpay_phys_jan <- genpay_phys[grep("01/", genpay_phys$Date_of_Payment), ]
+genpay_phys_feb <- genpay_phys[grep("02/", genpay_phys$Date_of_Payment), ]
+genpay_phys_mar <- genpay_phys[grep("03/", genpay_phys$Date_of_Payment), ]
+genpay_phys_apr <- genpay_phys[grep("04/", genpay_phys$Date_of_Payment), ]
+genpay_phys_may <- genpay_phys[grep("05/", genpay_phys$Date_of_Payment), ]
+genpay_phys_jun <- genpay_phys[grep("06/", genpay_phys$Date_of_Payment), ]
+genpay_phys_jul <- genpay_phys[grep("07/", genpay_phys$Date_of_Payment), ]
+genpay_phys_aug <- genpay_phys[grep("08/", genpay_phys$Date_of_Payment), ]
+genpay_phys_sep <- genpay_phys[grep("09/", genpay_phys$Date_of_Payment), ]
+genpay_phys_oct <- genpay_phys[grep("10/", genpay_phys$Date_of_Payment), ]
+genpay_phys_nov <- genpay_phys[grep("11/", genpay_phys$Date_of_Payment), ]
+genpay_phys_dec <- genpay_phys[grep("12/", genpay_phys$Date_of_Payment), ]
+
+genpay_1000$Date_of_Payment <- as.Date(genpay_1000$Date_of_Payment, "%m/%d/%Y")
+
 
 library(RMySQL)
 conn_cms <- dbConnect(MySQL(), user = "data_hacker", 
@@ -6,9 +33,14 @@ conn_cms <- dbConnect(MySQL(), user = "data_hacker",
                       password = "hack_pw",
                       port = 3306)
 
-teaching_hospitals <- dbGetQuery(conn_cms, "select *
-                                 from CMS_open_payments_2014.general_payment_data cms2014
-                                 where cms2014.Teaching_Hospital_ID IS NOT NULL;")
+# Select all rows where payment was made to physicians
+phys_jan <- dbGetQuery(conn_cms, "select cms2014.Physician_Profile_ID, cms2014.Physician_First_Name,
+                                         cms2014.Physician_Middle_Name, cms2014.Physician_Last_Name,
+                                         cms2014.Physician_Name_Suffix, cms2014.Recipient_Zip_Code,
+                                         cms2014.Physician_Primary_Type, cms2014.Physician_Specialty
+                                  from CMS_open_payments_2014.general_payment_data cms2014
+                                  where cms2014.Teaching_Hospital_ID IS NULL
+                                  and cms2014.Date_of_Payment LIKE '01/%';")
 
 write.csv(teaching_hospitals, "CMS 2014 General Payments - Teaching Hospitals.csv")
 
