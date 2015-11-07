76491a5495144679079f0fc4322766f5a0561412
arnoldlcl <arnoldlindlau@gmail.com>
2015-11-07 17:45:58 -0500

    Revised code for generating and writing Physician Info table

--- rmysql_alcl.R
+++ rmysql_alcl.R
@@ -1,3 +1,6 @@
+# The following code takes the CMS 2014 General Payments Details file, 5.5 GB,
+# and splits it into smaller tables related by a primary key.
+
 setwd("D:/GBWD")
 
 library(data.table) # The fread() function in the data.table library is much faster than read.csv(),
@@ -12,6 +15,8 @@ library(data.table) # The fread() function in the data.table library is much fas
 # Record_ID will serve as the primary key
 # Teaching_Hospital_ID is only in the table so we can subset: it'll be deleted later on
 
+# This table provides information on the physician involved with each financial transaction
+
 # Variable details:
 
 # Physician_Profile_ID: Unique ID for physician receiving payment
@@ -25,32 +30,29 @@ library(data.table) # The fread() function in the data.table library is much fas
 # Physician_Specialty: Specialty of physician. Not all physicians have a specialty
 # Record_ID: Unique ID for each financial transaction
 
-genpay_physinfo_p <- fread("CMS 2014/CMS 2014 General Payments Details.csv", 
-                         colClasses = c("NULL", "character", "NULL", "character", rep("NULL", 6),
-                                        rep("character", 8), rep("NULL", 25), "character", 
-                                        rep("NULL", 19)))
+genpay_phys <- fread("CMS 2014/CMS 2014 General Payments Details.csv", 
+                     colClasses = c("NULL", "character", "NULL", "character", rep("NULL", 6),
+                                    rep("character", 8), rep("NULL", 25), "character", 
+                                    rep("NULL", 19)))
 
 # Remove the 40,000-odd rows that refer to payments made to teaching hospitals
-genpay_physinfo_p <- genpay_physinfo_p[Teaching_Hospital_ID == "", ]
-genpay_physinfo_p[, Teaching_Hospital_ID := NULL] # Drop the Teaching_Hospital_ID column
+genpay_phys <- genpay_phys[Teaching_Hospital_ID == "", ]
+genpay_phys[, Teaching_Hospital_ID := NULL] # Drop the Teaching_Hospital_ID column
 
-# Ad hoc function to write genpay_physinfo to a new .csv file, in chunks
-write_genpay_physinfo <- function(d) {
-  for(i in 1:107) {
-    write.table(d[i:(i + 99999), ], 
+# Ad hoc function to write genpay_phys to a new .csv file, in chunks
+write_genpay <- function(d) {
+  for(i in 0:106) {
+    write.table(d[((i * 100000) + 1):((i + 1) * 100000)], 
                 "CMS 2014 General Payments Details - Physician Info.csv", append = TRUE, sep = ",")
   }
   write.table(d[10700001:10768319, ], 
                 "CMS 2014 General Payments Details - Physician Info.csv", append = TRUE, sep = ",")
 }
 
-write_genpay_physinfo(genpay_physinfo_p) # Write the physician info table to disk
+write_genpay(genpay_phys) # Write the physician info table to disk
+rm(genpay_phys)
 
-# Read the following columns of the General Payments Details .csv file:
-# Teaching_Hospital_ID, Teaching_Hospital_Name, Recipient_City, Recipient_State,
-# Recipient_Zipcode, Record_ID
-
-# Record_ID will serve as the primary key
+# This table provides information on the teaching hospital involved with each financial transaction
 
 # Variable details:
 
@@ -61,13 +63,31 @@ write_genpay_physinfo(genpay_physinfo_p) # Write the physician info table to dis
 # Recipient_Zip_Code: 9-digit zip code for location of physician's primary practice
 # Record_ID: Unique ID for each financial transaction
 
-genpay_physinfo_th <- fread("CMS 2014/CMS 2014 General Payments Details.csv",
-                            colClasses = c("NULL", rep("character", 2), rep("NULL", 7), 
-                                           rep("character", 3), rep("NULL", 30), "character", 
-                                           rep("NULL", 19)))
+genpay_th <- fread("CMS 2014/CMS 2014 General Payments Details.csv",
+                    colClasses = c("NULL", rep("character", 2), rep("NULL", 7), 
+                                    rep("character", 3), rep("NULL", 30), "character", 
+                                    rep("NULL", 19)))
 
-genpay_physinfo_th <- genpay_physinfo_th[Teaching_Hospital_ID != "", ]
+genpay_th <- genpay_th[Teaching_Hospital_ID != "", ]
 
 # Write the teaching hospital info table to disk
-write.csv(genpay_physinfo_th, "CMS 2014 General Payments Details - Teaching Hospitals Info.csv")
+write.csv(genpay_th, "CMS 2014 General Payments Details - Teaching Hospitals Info.csv")
+rm(genpay_th)
 
+# This table provides information about the financial transactions themselves
+
+# Teaching_Hospital_ID: Unique ID for teaching hospital that received payment
+# Physician_Profile_ID: Unique ID for physician receiving payment
+# Total_Amount_of_Payment_USDollars: Payment amount in USD; if in foreign currency, converted to USD
+# Date_of_Payment: Date of payment (if many payments, date of first payment)
+# Number_of_Payments_Included_in_Total_Amount: Number of discrete payments
+# Form_of_Payment_or_Transfer_of_Value: Method of payment (e.g. cash? in-kind?)
+# Nature_of_Payment_or_Transfer_of_Value: Nature of payment (e.g. consulting fee?)
+# Record_ID: Unique ID for each financial transaction
+
+genpay_trans <- fread("CMS 2014/CMS 2014 General Payments Details.csv",
+                      colClasses = c("NULL", "character", "NULL", "character", 
+                                     rep("NULL", 24), rep("character", 5), rep("NULL", 10), 
+                                     "character", rep("NULL", 19)))
+
+# Ad hoc function to write genpay_trans to a new .csv file, in chunks
